7454ae991b81a0c3e729da4c01b2c85daa2a6189
Fufaika <168428575+FufaikaOrig@users.noreply.github.com>
2024-05-04 17:04:07 +0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -17,3 +17,17 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 500 - 80:
             self.rect.y += self.speed
 
+    def update_r(self):
+
+        keys = key.get_pressed()
+
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 700 - 80:
+            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+
+
