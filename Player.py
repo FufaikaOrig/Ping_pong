eae6988a02952245acd027222cbfc0975706e50a
Fufaika <168428575+FufaikaOrig@users.noreply.github.com>
2024-05-11 17:10:12 +0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -8,19 +8,6 @@ class Player(GameSprite):
 
         keys = key.get_pressed()
 
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 700 - 80:
-            self.rect.x += self.speed
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 500 - 80:
-            self.rect.y += self.speed
-
-    def update_r(self):
-
-        keys = key.get_pressed()
-
         if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
         if keys[K_d] and self.rect.x < 700 - 80:
@@ -28,6 +15,19 @@ class Player(GameSprite):
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+
+    def update_r(self):
+
+        keys = key.get_pressed()
+
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 700 - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
             self.rect.y += self.speed
 
 
